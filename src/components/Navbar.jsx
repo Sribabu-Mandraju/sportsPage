9ef3ba077c78a38d5cd3f2ff7f02c5e6c2d1f897
Dhanu <avuladharaneeswarreddy123@gmail.com>
2024-12-10 12:02:55 +0530

    newchanges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-
-const Navbar = () => {
-  return (
-    <div className="navbar w-full bg-black h-[100px] flex justify-around">
+import React from 'react';
+import { FaLocationDot } from "react-icons/fa6";
+const Navbar = () => {return (
+    <div className="navbar w-full bg-black h-[100px] flex justify-around items-center">
         <div className="logo h-[50px] w-[170px]">
         <img class="logo-img h-full w-full" src="https://otcdn.virginia.us-east-1.oddstrader.com/OT-LogoWhiteGreen.svg" alt="Logo"/>
         </div>
@@ -18,14 +17,34 @@ const Navbar = () => {
             </div>
         </div>
         <div className="profile">
-            <div className="location">
-                <select name="location" id="location">
-                        <option></option>
-                        <option></option>
-                        <option></option>
-                        <option></option>
-                        <option></option>
-                        <option></option>
+            <div className="location flex items-center gap-[10px]">
+            <FaLocationDot className='text-[lime] outline-none'/>
+                <select name="location" id="location" className='outline-none bg-[black] text-white'>
+                        <option>World</option>
+                        <option>New Jersey</option> 
+                        <option>New York</option> 
+                        <option>Pennsylvania</option> 
+                        <option>Indiana</option>
+                        <option>Iowa
+         </option>                   <option>Illinois
+     </option>                       <option>Virginia
+     </option>                       <option>West Virginia
+</option>                            <option>Tennessee
+    </option>                        <option>Connecticut
+  </option>                          <option>Michigan
+     </option>                       <option>Arizona
+      </option>                      <option>Louisiana
+    </option>                        <option>Wyoming
+      </option>                      <option>Oregon
+       </option>                     <option>Kansas
+       </option>                     <option>Washington DC
+</option>                            <option>Maryland
+     </option>                       <option>Ohio
+         </option>                   <option>North Carolina</option>
+                            <option>Massachusetts
+</option>                            <option>Mississippi 
+ </option>
+                            
                 </select>
             </div>
         </div>
