a78e47d0c3bd909c401d4a25daedcfd9c61d671d
sribabu <sribabumandraju@gmail.com>
2024-12-10 14:58:24 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { Tabs } from "./components/Tabs";
+import "./App.css";
+
 const App = () => {
   return (
     <div>
       <div className="text-center">
-      <Navbar/>
-      <Tabs/>
+        <Navbar />
+        <Tabs />
       </div>
     </div>
   );
