e1c5ab0552459a44f1fa1fe5e8e2531d07661557
sribabu <sribabumandraju@gmail.com>
2024-12-11 08:03:13 +0530

    sd

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from "react";
 import { IoIosStarOutline } from "react-icons/io";
 export const Table = () => {
   return (
     <div>
-    <div className="event-status flex gap-[10px]">
-    <IoIosStarOutline className='text-[orange] text-[20px]' />
-    <h1><span className='font-semibold pr-[10px]'>TUE 12/10</span> 6:30PM</h1>
+      <div className="event-status flex gap-[10px]">
+        <IoIosStarOutline className="text-[orange] text-[20px]" />
+        <h1>
+          <span className="font-semibold pr-[10px]">TUE 12/10</span> 6:30PM
+        </h1>
+      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <h1>Title</h1>
+              <h3>Time</h3>
+            </td>
+            <td>
+              <select></select>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-        <table>
-            <tbody>
-                <tr>
-                   <td>
-                     <h1>Title</h1>
-                     <h3>Time</h3>
-                   </td>
-                   <td>
-                    <select></select>
-                   </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-  )
-}
+  );
+};
